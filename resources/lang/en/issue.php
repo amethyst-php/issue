c86aa9ef2d7bf0e833cda8e15daf1441796beb6b
railken <railken@protonmail.ch>
2019-04-06 22:41:56 +0200

    add lang

---
+++ resources/lang/en/issue.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'name' => 'Issue',
+    'description' => 'Issue',
+    'attributes' => [
+        'status_id' => [
+            'label' => 'Status'
+        ],
+        'tag' => [
+            'label' => 'Tags'
+        ]
+    ]
+]
---
+++ resources/lang/it/issue.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'name' => 'Issue',
+    'description' => 'Issue',
+    'attributes' => [
+        'status_id' => [
+            'label' => 'Stato'
+        ],
+        'tag' => [
+            'label' => 'Tags'
+        ]
+    ]
+]
