b4f00c978dfc8f272aabf1c3f907296bff5312e2
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/IssueManager.php
+++ src/Managers/IssueManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Issue newEntity()
+ * @method \Amethyst\Schemas\IssueSchema getSchema()
+ * @method \Amethyst\Repositories\IssueRepository getRepository()
+ * @method \Amethyst\Serializers\IssueSerializer getSerializer()
+ * @method \Amethyst\Validators\IssueValidator getValidator()
+ * @method \Amethyst\Authorizers\IssueAuthorizer getAuthorizer()
+ */
 class IssueManager extends Manager
 {
     use ConfigurableManager;
