661f4045fdbba22bd8bee7081509495d0edd47ce
railken <railken@protonmail.ch>
2019-07-14 13:09:40 +0200

    fix style

--- src/Managers/IssueManager.php
+++ src/Managers/IssueManager.php
@@ -6,12 +6,12 @@ use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
 /**
- * @method \Amethyst\Models\Issue newEntity()
- * @method \Amethyst\Schemas\IssueSchema getSchema()
+ * @method \Amethyst\Models\Issue                 newEntity()
+ * @method \Amethyst\Schemas\IssueSchema          getSchema()
  * @method \Amethyst\Repositories\IssueRepository getRepository()
- * @method \Amethyst\Serializers\IssueSerializer getSerializer()
- * @method \Amethyst\Validators\IssueValidator getValidator()
- * @method \Amethyst\Authorizers\IssueAuthorizer getAuthorizer()
+ * @method \Amethyst\Serializers\IssueSerializer  getSerializer()
+ * @method \Amethyst\Validators\IssueValidator    getValidator()
+ * @method \Amethyst\Authorizers\IssueAuthorizer  getAuthorizer()
  */
 class IssueManager extends Manager
 {
